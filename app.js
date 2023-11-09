51638d9a1091ac9832d0f6799a3b4757d6a082b4
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-11-09 14:20:44 +0100

    done

--- app.js
+++ app.js
@@ -6,21 +6,34 @@ import {
 
 createApp({
   setup() {
+    //para almacenar la lista de números
     const numbers = ref([]);
-
+// función para añadir un número aleatorio
     const addNumber = () => {
       const randomNumber = parseInt(Math.random() * 100) + 1;
       numbers.value.push(randomNumber);
     };
-
+//crear una computed property para filtrar los números
     const evenNumbers = computed(() => {
-      return numbers.value.filter((n) => n); // TODO: Modifica el método filter para quedarte únicamente con los números pares del array de numbers
+      return numbers.value.filter((n) => n % 2 === 0); // TODO: Modifica el método filter para quedarte únicamente con los números pares del array de numbers
     });
+    const oddNumbers = computed(() => {
+      return numbers.value.filter((n) => n % 2 !== 0);
+  });
 
+    const lenghtArrayText = computed(() => {
+      return numbers.value.length % 2 === 0 ? 'Par' : Impar;
+    });
+    
+
+
+    //devolver las referencias y funciones que se necesitan en el template 
     return {
       numbers,
       addNumber,
+      oddNumbers,
       evenNumbers,
+      lenghtArrayText
     };
   },
 }).mount("#app");
